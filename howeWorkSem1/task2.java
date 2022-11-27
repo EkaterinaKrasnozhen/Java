eea6267b361872b6b551f66175097e48b0f5ca80
kate <01redcat@mail.ru>
2022-11-27 17:12:27 +0300

    простые числа от 1 до 1000

--- howeWorkSem1/task2.java
+++ howeWorkSem1/task2.java
@@ -5,12 +5,11 @@ package howeWorkSem1;
 public class task2 {
     public static void main(String[] args) {
         boolean status;
-        int num = 22;
+        int num = 1000;
         for (int i = 2; i < num+1; i++) {
         status = true;
             for (int j = 2; j <= i/j; j++) {
                 if ((i%j) == 0)
-                //System.out.printf("i = %d, j = %d ", i, j);
                 status = false;
             }
             if (status == true)
