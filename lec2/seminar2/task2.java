9071d8233ccae242c4dc890dc5b982bb8b14570e
kate <01redcat@mail.ru>
2022-11-29 19:06:11 +0300

    RLE сжатие строки

---
+++ lec2/seminar2/task2.java
@@ -0,0 +1,31 @@
+// Задание 2.
+
+// Напишите метод, который сжимает строку по правилу: 
+// На входе: aaaabbbcdd
+// На выходе: a4b3cd2
+
+public class task2 {
+    public static void main(String[] args) {
+        String s = "aaaabbbcddx";
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        Character ch = s.charAt(0);
+
+        int j = s.length()-1;
+        for (int index = 1; index < s.length(); index++) {
+            if (s.charAt(index) == ch) {
+                count++;    
+            } 
+            else { 
+                sb.append(count);
+                sb.append(ch);
+                ch = s.charAt(index);
+                count = 1;    
+            }
+            if (index == s.length()-1) {
+                sb.append(count);
+                sb.append(ch);
+            }
+        }System.out.println(sb);
+    }
+}
