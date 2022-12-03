8cdd928334ea983b30378bac0e8711dfd749789c
kate <01redcat@mail.ru>
2022-12-03 17:58:59 +0300

    Random, sort Comparator

---
+++ seminar3/task1.java
@@ -0,0 +1,24 @@
+// 1. Заполнить список десятью случайными числами.
+// Отсортировать список методом sort() и вывести его на экран.
+
+package seminar3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class task1 {
+    public static void main(String[] args) {
+        List<Integer> l = new ArrayList<Integer>(10);
+        for (int index = 0; index < 10; index++) {
+            Random rnd = new Random();
+            int j = rnd.nextInt(0,100);
+            l.add(j);
+        }
+        System.out.println(l);
+        System.out.println();
+        l.sort(null);
+        System.out.println(l);
+    }
+    
+}
