b68ab4f93335d4edc7a56d54e79e9ed31f06554d
kate <01redcat@mail.ru>
2022-12-04 01:18:32 +0300

    task 3 todo

--- seminar3/task3.java
+++ seminar3/task3.java
@@ -4,8 +4,42 @@
 
 package seminar3;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class task3 {
+
     public static void main(String[] args) {
+        ArrayList<String> s = new ArrayList<String>();
+        s.add("null");
+        s.add("1");
+        s.add("3");
+        s.add("qwe");
+        String [] s1 = new String[s.size()];
+        StringBuilder sb = new StringBuilder();
+        s.toArray(s1);
+        int [] res = Numeric(s1);
+        for (int w : res) {
+            System.out.println(w);
+        }
         
+
+        Iterator<String> col1 = s.iterator();
+        while (col1.hasNext()) {
+            
+           
+            
+        }
+    }
+    public static int [] Numeric(String [] s2) {
+        int [] n = new int[s2.length];
+        try {
+ 
+            
+            
+        } catch (NumberFormatException e) {
+            //
+        }
+        return n;
     }
 }
