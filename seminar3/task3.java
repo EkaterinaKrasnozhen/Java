95ae340ba04459d063fb8c75903ed4ec8bffd72d
kate <01redcat@mail.ru>
2022-12-03 21:52:46 +0300

    task 3 todo

---
+++ seminar3/task3.java
@@ -0,0 +1,11 @@
+// Создать список типа ArrayList<String>.
+// Поместить в него как строки, так и целые числа.
+// Пройти по списку, найти и удалить целые числа.
+
+package seminar3;
+
+public class task3 {
+    public static void main(String[] args) {
+        
+    }
+}
