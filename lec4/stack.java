1e8b6c7918433402576122dea4624f6ab3e3d577
kate <01redcat@mail.ru>
2022-12-04 17:06:15 +0300

    Stack

--- lec4/stack.java
+++ lec4/stack.java
@@ -1,55 +1,67 @@
 package lec4;
 import java.util.Stack;
 
+// public class stack {
+//     private static boolean isDigit(String s) throws NumberFormatException {
+//         try {
+//             Integer.parseInt(s);
+//             return true;
+//         } catch (NumberFormatException e) {
+//             return false;
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         // (1+2*3*4)*(10/5) - 20
+//         // 1 2 3 * 4 * + 10 5 / * 20 -
+        
+//         //var exp = "20 30 - 10 *".split(" "); // (20-30)*10
+//         var exp = "1 2 + 3 *".split(" "); // (1 + 2) * 3
+        
+//         //var exp = "1 2 3 * +".split(" "); // 1 + 2 * 3
+//         int res = 0;
+//         System.out.println(exp);
+        
+//         Stack<Integer> st = new Stack<>();
+//         for (int i = 0; i < exp.length; i++) {
+
+//             if (isDigit(exp[i])) {
+//                 st.push(Integer.parseInt(exp[i]));
+//             } else {
+//                 switch (exp[i]) {
+//                     case "+":
+//                         res = st.pop() + st.pop();
+//                         st.push(res);
+//                         break;
+//                     case "-":
+//                         res = -st.pop() + st.pop();
+//                         st.push(res);
+//                         break;
+//                     case "*":
+//                         res = st.pop() * st.pop();
+//                         st.push(res);
+//                         break;
+//                     case "/":
+//                         res =  st.pop()/ st.pop();
+//                         st.push(res);
+//                         break;
+//                     default:
+//                         break;
+//                 }
+//             }
+//         }
+//         System.out.printf("%d\n", st.pop());
+//     }
+// }
+
 public class stack {
-    private static boolean isDigit(String s) throws NumberFormatException {
-        try {
-            Integer.parseInt(s);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
     public static void main(String[] args) {
-        // (1+2*3*4)*(10/5) - 20
-        // 1 2 3 * 4 * + 10 5 / * 20 -
-        
-        //var exp = "20 30 - 10 *".split(" "); // (20-30)*10
-        var exp = "1 2 + 3 *".split(" "); // (1 + 2) * 3
-        
-        //var exp = "1 2 3 * +".split(" "); // 1 + 2 * 3
-        int res = 0;
-        System.out.println(exp);
-        
         Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < exp.length; i++) {
-
-            if (isDigit(exp[i])) {
-                st.push(Integer.parseInt(exp[i]));
-            } else {
-                switch (exp[i]) {
-                    case "+":
-                        res = st.pop() + st.pop();
-                        st.push(res);
-                        break;
-                    case "-":
-                        res = -st.pop() + st.pop();
-                        st.push(res);
-                        break;
-                    case "*":
-                        res = st.pop() * st.pop();
-                        st.push(res);
-                        break;
-                    case "/":
-                        res =  st.pop()/ st.pop();
-                        st.push(res);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        System.out.printf("%d\n", st.pop());
+        st.push(1);
+        st.push(123);
+        st.push(12);
+        System.out.println(st.pop());//12
+        System.out.println(st.pop());//123
+        System.out.println(st.pop());//1
     }
 }
