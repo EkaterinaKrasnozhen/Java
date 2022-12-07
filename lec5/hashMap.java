45e37bcca2a4a3f9ddf747b101d48e6d1ce51868
kate <01redcat@mail.ru>
2022-12-07 14:22:01 +0300

    TreeMap

--- lec5/hashMap.java
+++ lec5/hashMap.java
@@ -3,12 +3,15 @@ import java.util.Map;
 
 public class hashMap {
     public static void main(String[] args) {
-        Map <Integer, String> db = new HashMap<>();
+        Map <Integer, String> db = new HashMap<>(); // по умолчанию 16 элементов
+        //Map <Integer, String> db = new HashMap<>(9, 1.0f); можно 0.7 -> тогда 70%, будет увеличиваться, 
+        //когда 9 заполнятся на 100 процентов, обычно при 75-80%
         db.put(1, "one");
         db.putIfAbsent(2, "two");
         System.out.println(db);
         for (var item : db.entrySet()) {
             System.out.printf("[%d: %s]\n", item.getKey(), item.getValue());
         }
+
     }
 }
---
+++ lec5/treeMap.java
@@ -0,0 +1,26 @@
+import java.util.TreeMap;
+
+public class treeMap {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> tMap = new TreeMap<>();
+        tMap.put(1, "one");
+        tMap.put(2, "two");
+        tMap.put(8, "eight");
+        tMap.put(6, "six");
+        System.out.println(tMap);//{1=one, 2=two, 6=six, 8=eight} по возрастанию добавил
+        tMap.put(2, "two"); System.out.println(tMap.descendingKeySet());//[8, 6, 2, 1]
+        tMap.put(2, "two"); System.out.println(tMap.descendingMap());//{8=eight, 6=six, 2=two, 1=one}
+    }
+}
+
+
+//TreeMap деревья
+//get(k) - получить значение
+//tailMap() - показать значение больше, чем
+//headMap() - меньше, чем
+//put(k,v) - добавить пару
+//remove(k) - удаоить пару
+//descendingKeySet(v) - получить исключительно сами ключи
+//descendingMap() - в обратном порядке
+//lastEntry() - 
+//firstEntry() -
