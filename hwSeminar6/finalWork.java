c56cfb235571696904aa75d5bde5f80adc6f48dc
kate <01redcat@mail.ru>
2022-12-13 16:18:32 +0300

    создание класса, добавление характеристик, фильтр по критериям

--- hwSeminar6/finalWork.java
+++ hwSeminar6/finalWork.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class finalWork {
     public static void main(String[] args) {
         noteBooks nb1 = new noteBooks(1, "Apple", "MacBook Pro 16", "ios", "silver", 16, 512, 120000);
-        noteBooks nb2 = new noteBooks(2, "Acer", "Aspire a515", "windows", "black", 16, 512, 98000);
+        noteBooks nb2 = new noteBooks(2, "Acer", "Aspire a515", "windows", "black", 8, 512, 98000);
         noteBooks nb3 = new noteBooks(3, "Asus", "X415Ea", "linux", "red", 8, 256, 40000); 
         noteBooks nb4 = new noteBooks(4, "HP", "15s-eq2136ur", "windows", "silver", 8, 256, 77000);
         noteBooks nb5 = new noteBooks(5, "Lenovo", "IdeaPad", "windows", "black", 16, 256, 80000);
@@ -18,27 +18,24 @@ public class finalWork {
         Map <Integer, String> dbFirm = new HashMap<>();
         Map <String, Integer> dbColor = new LinkedHashMap<>();
         Map <Integer, Integer> dbRam = new HashMap<>();
-        Map <Integer, Integer> dbPrice = new HashMap<>();
         Map <String, Integer> testOs = new LinkedHashMap<>();
         int count = 1;
         int count1 = 1;
+        int count2 = 1;
         for  (noteBooks n : nbs){
             dbFirm.put(n.id, n.firm);            
             dbColor.putIfAbsent(n.color, count1++);
-            dbRam.put(n.id, n.ram);
-            dbPrice.put(n.id, n.price);
+            dbRam.putIfAbsent(n.ram, count2++);
             testOs.putIfAbsent(n.os, count++);
-        } 
-        // System.out.println(dbColor); 
-        // System.out.println(testOs);  
-        getValueFromUser(dbFirm, testOs, dbColor, nbs);
+        }  
+        getValueFromUser(dbFirm, testOs, dbColor, dbRam, nbs);
     }
 
 
-    public static void getValueFromUser(Map <Integer, String> firm, Map <String, Integer> os, Map <String, Integer> c, noteBooks[] nb) {
+    public static void getValueFromUser(Map <Integer, String> firm, Map <String, Integer> os, Map <String, Integer> c, Map <Integer, Integer> ram,  noteBooks[] nb) {
         Scanner iScanner = new Scanner(System.in);  
         while (true) {
-            System.out.printf("Критерии фильтрации ноутбуков: 1 - Фирма\n 2 - ОС\n 3 - цвет\n 4 - RAM\n 5 - цена\n ");
+            System.out.printf("Критерии фильтрации ноутбуков: \n 1 - Фирма\n 2 - ОС\n 3 - цвет\n 4 - RAM\n 5 - цена\n ");
             String get_user = iScanner.nextLine();
             if (get_user.equals("1")) {
                 noteBooks.getFirm(firm, nb);
@@ -48,6 +45,12 @@ public class finalWork {
             }
             else if (get_user.equals("3")) {
                 noteBooks.getCol(c, nb);
+            }
+            else if(get_user.equals("4")) {
+                noteBooks.getRam(ram, nb);
+            }
+            else if(get_user.equals("5")) {
+                noteBooks.getPrice(nb);
             }
             else if (get_user.equals("0")){
                 System.out.printf("buy");
--- hwSeminar6/noteBooks.java
+++ hwSeminar6/noteBooks.java
@@ -57,30 +57,44 @@ public class noteBooks {
     }
     
 
-    @Override
+    @Override //вывести ноутбук со всеми характеристиками
     public String toString() {
         return String.format("id: %d, firm: %s, model: %s, os: %s, color: %s, ram: %s, hd: %d, price: %d", id, firm, model, os, color, ram, hd, price);  
     }
 
 
-    public static StringBuilder getByFirm(noteBooks [] nbooks, int num) {//метод добавления в строку по фирме
+    public static StringBuilder getByFirm(noteBooks [] nbooks, int num) {//метод доб-я характеристик объектов в строку по фирме
         StringBuilder res = new StringBuilder();
+        int count = 0;
         for (noteBooks b : nbooks) {
             if (b.id == num) {
-                res.append(b.model + "\n" + b.os + "\n" + b.color + "\n" + b.hd + "\n" + b.ram + "\n" + b.price);
+                res.append("\n" + "Вариант N" + ++count + "\n" + "Модель: " + b.model + "\n" + "ОС: " + b.os + "\n" + "цвет: " + b.color + "\n" + b.hd + "\n" + "RAM: " + b.ram + "\n" + "Цена: " + b.price + "\n");
             }
         }
         return res;
     }
 
 
-    public static StringBuilder getByOs(noteBooks [] nbooks, Integer user, Map <String, Integer> os) {//строка по ОС
-        StringBuilder res = new StringBuilder();//получаем строку с ноутбуками выбранной ОС
-        for (var item : os.entrySet()) {
+    public static StringBuilder getByPrice(noteBooks [] nbooks, int min, int max) {//метод добавления в строку по цене
+        StringBuilder res = new StringBuilder();
+        int count = 0;
+        for (noteBooks b : nbooks) {
+            if ((b.price <= max) && (min <= b.price)) {
+                res.append("\n" + "Вариант N" + ++count + "\n" + "Фирма: " + b.firm +  "Модель: " + b.model + "\n" + "ОС: " + b.os + "\n" + "цвет: " + b.color + "\n" + "HD: " + b.hd + "\n" + "RAM: " + b.ram + "\n" + "Цена: " + b.price + "\n");
+            }
+        }
+        return res;
+    }
+    
+    
+    public static StringBuilder getByRam(noteBooks [] nbooks, Integer user, Map <Integer, Integer> ram) {//строка по RAM
+        StringBuilder res = new StringBuilder();
+        int count = 0;
+        for (var item : ram.entrySet()) {
             for (noteBooks b : nbooks) {
                 if (item.getValue().equals(user)) {//находим по выбору пользователя нужное значение с ключом
-                    if (b.os == item.getKey()) {//по найденному ключу находим нужную ОС
-                        res.append(b.firm + "\n" + b.model + "\n" + b.color + "\n" + b.hd + "\n" + b.ram + "\n" + b.price); 
+                    if (b.ram == item.getKey()) {//по найденному ключу находим нужную RAM
+                        res.append("\n" + "Вариант N" + ++count + "\n" + "Фирма: " + b.firm + "\n" + "Модель: " + b.model + "\n" + "цвет: " + b.color + "\n" + "HD: " + b.hd + "\n" + "ОС: " + b.os + "\n" + "Цена: " + b.price + "\n"); 
                     }
                 }
             }
@@ -89,13 +103,30 @@ public class noteBooks {
     }
 
 
-    public static StringBuilder getByColor(noteBooks [] nbooks, Integer user, Map <String, Integer> col) {//строка по ОС
+    public static StringBuilder getByOs(noteBooks [] nbooks, Integer user, Map <String, Integer> os) {//строка по ОС
         StringBuilder res = new StringBuilder();//получаем строку с ноутбуками выбранной ОС
+        int count = 0;
+        for (var item : os.entrySet()) {
+            for (noteBooks b : nbooks) {
+                if (item.getValue().equals(user)) {//находим по выбору пользователя нужное значение с ключом
+                    if (b.os == item.getKey()) {//по найденному ключу находим нужную ОС
+                        res.append("\n" + "Вариант N" + ++count + "\n" + "Фирма: " + b.firm + "\n" + "Модель: " + b.model + "\n" + "цвет: " + b.color + "\n" + "HD: " + b.hd + "\n" + "RAM: " + b.ram + "\n" + "Цена: " + b.price + "\n"); 
+                    }
+                }
+            }
+        }
+        return res;   
+    }
+
+
+    public static StringBuilder getByColor(noteBooks [] nbooks, Integer user, Map <String, Integer> col) {//строка по цвету
+        StringBuilder res = new StringBuilder();//получаем строку с ноутбуками выбранного цвета
+        int count = 0;
         for (var item : col.entrySet()) {
             for (noteBooks b : nbooks) {
                 if (item.getValue().equals(user)) {//находим по выбору пользователя нужное значение с ключом
-                    if (b.color == item.getKey()) {//по найденному ключу находим нужную ОС
-                        res.append(b.firm + "\n" + b.model + "\n" + b.os + "\n" + b.hd + "\n" + b.ram + "\n" + b.price); 
+                    if (b.color == item.getKey()) {//по найденному ключу находим нужному цвету
+                        res.append("\n" + "Вариант N" + ++count + "\n" + "Фирма: " + b.firm + "\n" + "Модель: " + b.model + "\n" + "ОС: " + b.os + "\n" + "HD: " + b.hd + "\n" + "RAM: " + b.ram + "\n" + "Цена: " + b.price + "\n"); 
                     }
                 }
             }
@@ -115,6 +146,18 @@ public class noteBooks {
     }
 
 
+    public static void getPrice(noteBooks [] nbooks) {//метод, получ-ий диапазон цен от пользователя
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите через пробел диапазон цены от мин до макс: ");
+        String [] choice = iScanner.nextLine().split(" ");
+        int min = Integer.parseInt(choice[0]);
+        int max = Integer.parseInt(choice[1]);
+        StringBuilder res = getByPrice(nbooks, min, max);//вызываем метод формирования строки из нужных цен
+        System.out.println(res);
+        iScanner.close();
+    }
+
+
     public static void getOs(Map <String, Integer> i, noteBooks [] nbooks) {//метод, получ-ий знач-ие от польз. по ОС
         Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите цифру, соот-ую ОС: ");
@@ -126,12 +169,25 @@ public class noteBooks {
     }
 
 
-    public static void getCol(Map <String, Integer> i, noteBooks [] nbooks) {//метод, получ-ий знач-ие от польз. по ОС
+    public static void getRam(Map <Integer, Integer> i, noteBooks [] nbooks) {//метод, получ-ий знач-ие от польз. по Ram
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите цифру, соот-ую Ram: ");
+        for (var el : i.entrySet()) {
+            System.out.printf("%d Гб - %d\n", el.getKey(), el.getValue());
+        }
+        Integer choice = iScanner.nextInt();
+        StringBuilder res = getByRam(nbooks, choice, i);//вызываем метод формирования строки из нужных Ram
+        System.out.println(res);
+        iScanner.close();
+    }
+
+
+    public static void getCol(Map <String, Integer> i, noteBooks [] nbooks) {//метод, получ-ий знач-ие от польз. по цвету
         Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите цифру, соот-ую цвету: ");
         System.out.println(i);
         Integer choice = iScanner.nextInt();
-        StringBuilder res = getByColor(nbooks, choice, i);//вызываем метод формирования строки из нужных ОС
+        StringBuilder res = getByColor(nbooks, choice, i);//вызываем метод формирования строки из нужный цвет
         System.out.println(res);
         iScanner.close();
     }
